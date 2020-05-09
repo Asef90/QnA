11b4b2c497fd8037afa2ab3cdb7c47f87f17d32d
Asef90 <asef.dag90@gmail.com>
2020-05-09 23:52:59 +0300

    Add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :questions, only: [:show, :new, :create] do
-    resources :answers, only: [:show, :new, :create]
+  root 'questions#index'
+
+  devise_for :users
+
+  resources :questions do
+    resources :answers
   end
 end
