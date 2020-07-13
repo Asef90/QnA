463a62be56aab9de88bd8cdda6e4a7260fa1f26f
Asef90 <asef.dag90@gmail.com>
2020-07-14 01:51:47 +0300

    configure Sphinx every 30 min indexing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
   # before_action :authenticate_user!, except: %i[index show]
   before_action :set_question, only: %i[show update destroy]
-  
+
   include Voted
 
   authorize_resource
