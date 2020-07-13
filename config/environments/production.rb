8e0b3a878c4c41331206a2e55da61d2744b77930
Asef90 <asef.dag90@gmail.com>
2020-07-14 02:53:36 +0300

    set host to production env (action cable)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -126,3 +126,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+Rails.application.routes.default_url_options[:host] = '64.225.70.108'
